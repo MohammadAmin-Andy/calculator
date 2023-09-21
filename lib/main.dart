ccdf3c3f484381eb28ae0e1cef9c077833ae8907
Mamin <pg.mohammadamin@gmail.com>
2023-09-21 17:41:34 +0330

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,256 @@
+import 'package:calculator/constances.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    CalculatorApplicaton(),
+  );
+}
+
+class CalculatorApplicaton extends StatefulWidget {
+  const CalculatorApplicaton({Key? key}) : super(key: key);
+
+  @override
+  State<CalculatorApplicaton> createState() => _CalculatorApplicatonState();
+}
+
+bool isoperator(String text) {
+  var list = [
+    'ac',
+    'ce',
+    '%',
+    '/',
+    '*',
+    "-",
+    '+',
+    '=',
+  ];
+  for (var item in list) {
+    if (text == item) {
+      return true;
+    }
+  }
+  return false;
+}
+
+Color getBackgroundColor(String text) {
+  if (isoperator(text)) {
+    return backgroundGreyDark;
+  } else {
+    return backgroundGrey;
+  }
+}
+
+Color getTextColor(String text) {
+  if (isoperator(text)) {
+    return textGreen;
+  } else {
+    return textGrey;
+  }
+}
+
+class _CalculatorApplicatonState extends State<CalculatorApplicaton> {
+  var inputUser = '';
+
+  void buttonPressed(String text) {
+    setState(() {
+      inputUser += text;
+    });
+  }
+
+  Widget getnumbers(
+    String text1,
+    String text2,
+    String text3,
+    String text4,
+  ) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        TextButton(
+          style: TextButton.styleFrom(
+            shape: CircleBorder(
+              side: BorderSide(width: 0, color: Colors.transparent),
+            ),
+            backgroundColor: getBackgroundColor(text1),
+          ),
+          onPressed: () {
+            if (text1 == 'ac') {
+              setState(() {
+                if (inputUser.length > 0) {
+                  inputUser = '';
+                }
+              });
+            } else {
+              buttonPressed(text1);
+            }
+          },
+          child: Padding(
+            padding: EdgeInsets.all(3),
+            child: Text(
+              '$text1',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+                color: getTextColor(text1),
+              ),
+            ),
+          ),
+        ),
+        TextButton(
+          style: TextButton.styleFrom(
+            shape: CircleBorder(
+              side: BorderSide(width: 0, color: Colors.transparent),
+            ),
+            backgroundColor: getBackgroundColor(text2),
+          ),
+          onPressed: () {
+            if (text2 == 'ce') {
+              setState(() {
+                if (inputUser.length > 0) {
+                  inputUser = inputUser.substring(0, inputUser.length - 1);
+                }
+              });
+            } else {
+              buttonPressed(text2);
+            }
+          },
+          child: Padding(
+            padding: EdgeInsets.all(3),
+            child: Text(
+              '$text2',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+                color: getTextColor(text2),
+              ),
+            ),
+          ),
+        ),
+        TextButton(
+          style: TextButton.styleFrom(
+            shape: CircleBorder(
+              side: BorderSide(width: 0, color: Colors.transparent),
+            ),
+            backgroundColor: getBackgroundColor(text3),
+          ),
+          onPressed: () {
+            buttonPressed(text3);
+          },
+          child: Padding(
+            padding: EdgeInsets.all(3),
+            child: Text(
+              '$text3',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+                color: getTextColor(text3),
+              ),
+            ),
+          ),
+        ),
+        TextButton(
+          style: TextButton.styleFrom(
+            shape: CircleBorder(
+              side: BorderSide(width: 0, color: Colors.transparent),
+            ),
+            backgroundColor: getBackgroundColor(text4),
+          ),
+          onPressed: () {
+            buttonPressed(text4);
+          },
+          child: Padding(
+            padding: EdgeInsets.all(3),
+            child: Text(
+              '$text4',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+                color: getTextColor(text4),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                flex: 4,
+                child: Container(
+                  color: backgroundGreyDark,
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Text(
+                      '$inputUser',
+                      textAlign: TextAlign.end,
+                      style: TextStyle(
+                          color: textGreen,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 28),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 6,
+                child: Container(
+                  color: backgroundGrey,
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      getnumbers(
+                        'ac',
+                        'ce',
+                        '%',
+                        '/',
+                      ),
+                      getnumbers(
+                        '7',
+                        '8',
+                        '9',
+                        '*',
+                      ),
+                      getnumbers(
+                        '4',
+                        '5',
+                        '6',
+                        '-',
+                      ),
+                      getnumbers(
+                        '1',
+                        '2',
+                        '3',
+                        '+',
+                      ),
+                      getnumbers(
+                        '00',
+                        '0',
+                        '.',
+                        '=',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
